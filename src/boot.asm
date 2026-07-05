cdb1d27ec93401624329eb5558fd0d51245e8133
icr0409 <kalilinuxicr@gmail.com>
2026-07-05 20:32:47 -0300

    Update boot.asm

--- src/boot.asm
+++ src/boot.asm
@@ -1,18 +1,26 @@
-MB_MAGIC equ 0x1BADB002
-MB_FLAGS equ 0x00000000
+MB_MAGIC    equ 0x1BADB002
+MB_FLAGS    equ 0x00000000
 MB_CHECKSUM equ -(MB_MAGIC + MB_FLAGS)
 
 [BITS 32]
 
-section .text
+section .multiboot
+    align 4
     dd MB_MAGIC
     dd MB_FLAGS
     dd MB_CHECKSUM
 
+
+section .text
 global _start
 extern kmain
 
 _start:
-    cli
-    call kmain
-    hlt 
+    cli         
+    call kmain  
+    hlt         ; Trava o processador se o kmain retornar
+
+
+
+
+
