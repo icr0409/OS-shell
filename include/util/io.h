4c0a723d207e48876cefde00c47f8eac7ad906e4
Henrique Santos <hiqkrs@gmail.com>
2026-07-06 13:36:18 -0300

    feat(util): added IO related functions.

---
+++ include/util/io.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <stdint.h>
+
+static inline void outb(uint8_t value, uint16_t port) {
+	asm volatile("outb %0, %1" : : "a"(value), "d"(port));
+}
+
+static inline uint8_t inb(uint16_t port) {
+	uint8_t data;
+    asm volatile("inb %1, %0" : "=a"(data) : "d"(port));
+    return data;
+}
